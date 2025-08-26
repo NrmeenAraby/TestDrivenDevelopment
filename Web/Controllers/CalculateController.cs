44596e4de58c17f8eaf303b7cb1805e4cafafa99
NrmeenAraby <115955249+NrmeenAraby@users.noreply.github.com>
2025-08-27 02:35:42 +0300

    Adding Web API

---
+++ Web/Controllers/CalculateController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Domain;
+using System.Reflection.Metadata.Ecma335;
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculateController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<CalculateController> _logger;
+
+        public CalculateController(ILogger<CalculateController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("Add/{a}/{b}")]
+        public int Get(int a,int b)
+        {
+           var calculator = new Calculator();
+           return calculator.Sum(a,b);
+        }
+    }
+}
